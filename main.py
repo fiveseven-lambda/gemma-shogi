448fe81666663ab4e930c0f41bfca9ac37769825
fiveseven-lambda <fiveseven.lambda@gmail.com>
2025-08-05 17:45:56 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import sys
+import ollama
+
+MODEL = "gemma3:4b"
+
+
+def main():
+    while True:
+        try:
+            info = ollama.show(MODEL)
+            print(info.details)
+            break
+        except ollama.ResponseError as e:
+            print(e.error)
+            if e.status_code == 404:
+                print("Downloading", MODEL)
+                ollama.pull(MODEL)
+            else:
+                sys.exit(1)
+    messages = ["将棋をしましょう。お互いに実況・解説を交えながら、楽しく戦いましょう。こちらが先手です。7六歩"]
+    print("Initial prompt:")
+    print(messages[0])
+    turn = 0
+    while True:
+        stream = ollama.chat(
+            model=MODEL,
+            messages=[
+                {"role": "user" if i % 2 == turn else "assistant", "content": message}
+                for i, message in enumerate(messages)
+            ],
+            stream=True,
+        )
+        response = []
+        print(f"AI #{turn}:")
+        for chunk in stream:
+            content = chunk["message"]["content"]
+            print(content, end="", flush=True)
+            response.append(content)
+        print()
+        messages.append("".join(response))
+        turn = 1 - turn
+
+
+if __name__ == "__main__":
+    main()
